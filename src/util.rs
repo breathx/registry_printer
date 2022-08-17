43f5f922f2a0ef631ff20c18429a8ec6c342f13c
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-08-17 15:48:34 +0300

    Formatting and clippy resolving

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,14 @@
 /// Utility functions.
-
 use codec::Encode;
 use colored::*;
 use scale_info::{PortableRegistry, Registry};
 
 pub(crate) use scale_info::MetaType;
-pub(crate) use vec;
 
 #[macro_export]
 macro_rules! registry {
     ($t1:ty $(, $t:ty) *) => {
-        $crate::util::to_registry($crate::util::vec![
+        $crate::util::to_registry(vec![
             $crate::util::MetaType::new::<$t1>() $(,
                 $crate::util::MetaType::new::<$t>()
             ) *
@@ -29,7 +27,8 @@ pub fn print_registry(registry: Registry) {
     let registry: PortableRegistry = registry.into();
 
     let json = serde_json::to_string(&registry).expect("Failed to convert registry to JSON");
-    let pretty_json = serde_json::to_string_pretty(&registry).expect("Failed to convert registry to pretty JSON");
+    let pretty_json =
+        serde_json::to_string_pretty(&registry).expect("Failed to convert registry to pretty JSON");
     let bytes = registry.encode();
     let hex = hex::encode(bytes.clone());
 
@@ -42,8 +41,8 @@ pub fn print_registry(registry: Registry) {
     println!("{}\n", format!("{bytes:?}").green().italic());
 
     println!("  {} {}\n", ">>".blue(), "JSON".white().italic());
-    println!("{}\n", format!("{json}").green().italic());
+    println!("{}\n", json.green().italic());
 
     println!("  {} {}\n", ">>".blue(), "PRETTY JSON".white().italic());
-    println!("{}\n", format!("{pretty_json}").green().italic());
+    println!("{}\n", pretty_json.green().italic());
 }
